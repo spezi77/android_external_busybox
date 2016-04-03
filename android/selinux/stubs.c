0b1ba1e3d835006296ea9690a537f8a183c5b36b
spezi77 <spezi77@gmx.com>
2016-04-03 18:28:11 +0200

    Remove multiple implementation of "security_compute_relabel". This will solve a conflict between libselinux and libbusybox.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- android/selinux/stubs.c
+++ android/selinux/stubs.c
@@ -16,21 +16,6 @@ int get_default_context(const char* user,
 	return 0;
 }
 
-/* Compute a relabeling decision and set *newcon to refer to it.
-   Caller must free via freecon.
-   Stub not implemented in bionic, but declared in selinux.h */
-int security_compute_relabel(const char *scon UNUSED_PARAM,
-	const char *tcon,
-	security_class_t tclass UNUSED_PARAM,
-	char ** newcon)
-{
-	if (tcon)
-		*newcon = strdup(tcon);
-	if (!(*newcon))
-		return -1;
-	return 0;
-}
-
 /* Check a permission in the passwd class.
    Return 0 if granted or -1 otherwise. */
 int selinux_check_passwd_access(access_vector_t requested UNUSED_PARAM)
